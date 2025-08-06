328693aadcbd19cf1d16367b7fee123531034f9a
laeeq <laeeqhaq@gmail.com>
2025-08-06 15:21:18 +0600

    initial layout

--- app/page.tsx
+++ app/page.tsx
@@ -2,102 +2,89 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <>
+      <section className="dark-section bg-center w-full">
+        <div className="min-h-200 bg-dark py-40 px-20 flex flex-col gap-12">
+          <h1>Lorem ipsum dolor sit amet consectetur</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+            saepe quia, beatae voluptate culpa fuga.
+          </p>
+          <div className="flex gap-8">
+            <a href="" className="button bg-primary text-dark">
+              More info
+            </a>
+            <a href="" className="button">
+              More info
+            </a>
+          </div>
+          {/* <div>
+            <Image width={800} height={800} src={"/test-bg-1.jpg"} alt="" />
+          </div> */}
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </section>
+      <section className="dark-section bg-red-800 py-32 px-20 flex flex-col gap-8">
+        <h5 className="text-light/90">Who Are We?</h5>
+        <h2>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis.,
+          rem.
+        </h2>
+        <p className="">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
+          explicabo sapiente exercitationem in vero repudiandae corporis eius
+          dolorum quas eaque maiores aliquid, ab ea esse. Voluptatibus magnam
+          consequuntur sapiente minima!
+        </p>
+      </section>
+      <section className="dark-section bg-blue-700 py-32 px-20 flex flex-col gap-8">
+        <h5 className="text-light/90">What we can do for you</h5>
+        <h2>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis.,
+          rem.
+        </h2>
+        <p className="">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Doloribus illum sed
+          possimus, temporibus beatae esse nemo ullam dicta deleniti
+          exercitationem ipsam? Exercitationem ipsam eius magni corporis, iusto
+          ex quisquam maiores adipisci qui accusantium est quod, cupiditate
+          nobis nam debitis beatae.
+        </p>
+        <div className="flex flex-col gap-12">
+          <div className="card"></div>
+        </div>
+      </section>
+      <section className="dark-section bg-green-600 py-32 px-20 flex flex-col gap-8">
+        <h5 className="text-light/90">Heres what others think of us</h5>
+        <h2>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis.,
+          rem.
+        </h2>
+        <p className="">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Doloribus illum sed
+          possimus, temporibus beatae esse nemo ullam dicta deleniti
+          exercitationem ipsam? Exercitationem ipsam eius magni corporis, iusto
+          ex quisquam maiores adipisci qui accusantium est quod, cupiditate
+          nobis nam debitis beatae.
+        </p>
+        <div className="flex flex-col gap-12">
+          <div className="card"></div>
+        </div>
+      </section>
+      <section className="dark-section bg-amber-500 py-32 px-20 flex flex-col gap-8">
+        <h5 className="text-light/90">Get in touch with us</h5>
+        <h2>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis.,
+          rem.
+        </h2>
+        <p className="">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
+          explicabo sapiente exercitationem in vero repudiandae corporis eius
+          dolorum quas eaque maiores aliquid, ab ea esse. Voluptatibus magnam
+          consequuntur sapiente minima!
+        </p>
+      </section>
+    </>
   );
 }
