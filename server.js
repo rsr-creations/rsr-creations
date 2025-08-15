6c46db62fb65c8f6453a3c94f3098ac9feba6f25
RSR Creations <rsrcreationsbd@gmail.com>
2025-08-15 18:20:05 +0600

    created server script

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   const server = express();
-  server.all('*', (req, res) => handle(req, res));
+  server.all(/(.*)/, (req, res) => handle(req, res));
   server.listen(process.env.PORT || 3000, () => {
     console.log(`Ready on port ${process.env.PORT || 3000}`);
   });
