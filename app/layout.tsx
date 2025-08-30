afe92e2cb6e782d67cd398e469528f6c1e17ef2b
laeeq <laeeqhaq@gmail.com>
2025-08-30 11:58:59 +0600

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
-import type { Metadata } from "next";
-import { Montserrat, Open_Sans } from "next/font/google";
+import type {Metadata} from "next";
+import {Montserrat, Open_Sans} from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 
@@ -37,12 +37,12 @@ export default function RootLayout({
       <footer id="contact" className="dark-section bg-dark min-h-100 section">
         <div className="container">
           <h5 className="text-green-500">Get in touch with us</h5>
-          <div className="grid gap-1">
-            <p className="">Number: +880 1936 609 937</p>{" "}
-            <p>
+          <div className="grid gap-1 text-light/70">
+            <ul className="">Contact: +880 1936 609 937</ul>{" "}
+            <ul>
               Email: <span>rsrcreations@gmail.com</span>{" "}
-            </p>
-            <p>
+            </ul>
+            <ul>
               Our Facebook page:{" "}
               <a
                 href="https://www.facebook.com/profile.php?id=61578550110498"
@@ -50,8 +50,11 @@ export default function RootLayout({
               >
                 RSR Creations
               </a>
-            </p>
+            </ul>
           </div>
+          <ul className="text-md font-bold text-less-light">
+            Copyright 2025 @ RSR Creations | All Rights Reserved
+          </ul>
         </div>
       </footer>
     </>
